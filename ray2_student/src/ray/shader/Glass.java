d0471ee2e414f16c2847ab211ca933e527765e97
Yingchuan Hu <huyc09@gmail.com>
2013-12-10 19:47:56 -0500

    glass and glazed added

--- ray2_student/src/ray/shader/Glass.java
+++ ray2_student/src/ray/shader/Glass.java
@@ -41,6 +41,85 @@ public class Glass extends Shader {
 	public void shade(Color outIntensity, Scene scene, Ray ray, IntersectionRecord record, int depth) {
 		// TODO: fill in this function.
 		// You may find it helpful to create helper methods if the code here gets too long.
+		Vector3 outgoing = new Vector3();
+		outgoing.sub(ray.origin, record.location);
+		Vector3 outgoingNorm = new Vector3(outgoing);
+		outgoingNorm.normalize();
+		
+		Vector3 normalNorm = record.normal;
+		normalNorm.normalize();
+		
+		//Project outgoing ray to normal
+		double projection = outgoingNorm.dot(normalNorm);
+		
+		//Fresnel Coefficient using Schlick's Approx
+		double n_1, n_2;
+		if (projection > 0) { //outgoing is outside the material
+			n_1 = 1.0;
+			n_2 = refractiveIndex;
+		}
+		else if (projection < 0) { //outgoing is inside the material
+			n_1 = refractiveIndex;
+			n_2 = 1.0;
+			normalNorm.scale(-1.0); //make normal on the same side as outgoingNorm
+			normalNorm.normalize();
+			projection = outgoingNorm.dot(normalNorm);
+		}
+		
+		else { //this should never happen
+			//if the outgoing and the normal are perp., then the
+			//outgoing is implied not to intersect the surface, contradiction!
+			System.out.println("This happened!");
+			return;
+		}
+		
+		double r_0 = Math.pow(((n_2 - n_1)/(n_2 + n_1)), 2.0);
+		//Fresnel Coefficient
+		//r_0 + (1 - r_0)(1 - cos(theta))^5
+		//cos(theta) = (normalNorm dot outgoingNorm) / (||normalNorm|| ||outgoingNorm||)
+		
+		double fresnel = r_0 + (1.0 - r_0) * Math.pow(1.0 - projection, 5.0);
+		
+		Vector3 reflectedRayDir = new Vector3(normalNorm);
+		reflectedRayDir.scale(2.0);
+		reflectedRayDir.scale(projection);
+		reflectedRayDir.sub(outgoingNorm);
+		reflectedRayDir.normalize();
+		
+		Ray reflectedRay = new Ray();
+		reflectedRay.set(record.location, reflectedRayDir);
+		reflectedRay.makeOffsetRay();
+		Color reflectedColor = new Color();
+		RayTracer.shadeRay(reflectedColor, scene, reflectedRay, depth);
+		
+		
+		projection = outgoing.dot(normalNorm);
+		double determinant = 1 - (Math.pow(n_1, 2.0) * (1-Math.pow(projection, 2.0)))/(Math.pow(n_2, 2.0));
+		
+		if (determinant < 0.0) {
+			reflectedColor.scale(1.0);
+			outIntensity.set(reflectedColor);
+		} else {
+			reflectedColor.scale(fresnel);
+			
+			Vector3 transmittedVector = new Vector3(outgoing);
+			Vector3 subtractArgOne = new Vector3(normalNorm);
+			Vector3 subtractArgTwo = new Vector3(normalNorm);
+			
+			subtractArgOne.scale(projection);
+			transmittedVector.sub(subtractArgOne);
+			transmittedVector.scale(n_1/n_2);
+			
+			subtractArgTwo.scale(Math.sqrt(determinant));
+			transmittedVector.sub(subtractArgTwo);
+			
+			Ray refractedRay = new Ray();
+			refractedRay.makeOffsetRay();
+			reflectedRay.set(record.location, transmittedVector);
+			RayTracer.shadeRay(outIntensity, scene, refractedRay, depth);
+			outIntensity.scale(1-fresnel);
+			outIntensity.add(reflectedColor);
+		}
 		
 	}
 	
--- ray2_student/src/ray/shader/Glazed.java
+++ ray2_student/src/ray/shader/Glazed.java
@@ -8,6 +8,7 @@ import ray.math.Color;
 import ray.math.Vector3;
 import ray.RayTracer;
 
+
 public class Glazed extends Shader {
 
 	/**
@@ -44,5 +45,59 @@ public class Glazed extends Shader {
 		// TODO: fill in this function.
 		// You may find it helpful to create helper methods if the code here gets too long.
 		
+		/* Generate Reflected Ray */
+		Vector3 outgoing = new Vector3();
+		outgoing.sub(ray.origin, record.location);
+		Vector3 outgoingNorm = new Vector3(outgoing);
+		outgoingNorm.normalize();
+		
+		Vector3 normalNorm = record.normal;
+		normalNorm.normalize();
+		
+		//Project outgoing ray to normal
+		double projection = outgoingNorm.dot(normalNorm);
+		
+		//Fresnel Coefficient using Schlick's Approx
+		double n_1, n_2;
+		if (projection > 0) { //outgoing is outside the material
+			n_1 = 1.0;
+			n_2 = refractiveIndex;
+		}
+		else if (projection < 0) { //outgoing is inside the material
+			System.out.println("Outgoing is inside the material");
+			return ;
+		}
+		else { //this should never happen
+			//if the outgoing and the normal are perp., then the
+			//outgoing is implied not to intersect the surface, contradiction!
+			System.out.println("Outgoing is on the material!");
+			return ;
+		}
+		
+		double r_0 = Math.pow(((n_2 - n_1)/(n_2 + n_1)), 2.0);
+		//Fresnel Coefficient
+		//r_0 + (1 - r_0)(1 - cos(theta))^5
+		//cos(theta) = (normalNorm dot outgoingNorm) / (||normalNorm|| ||outgoingNorm||)
+		double fresnel = r_0 + (1.0 - r_0) * Math.pow(1.0 - projection, 5.0);
+		
+		//Reflected Ray = 2 * normalNorm * (normalNorm dot outgoingNorm) - outgoingNorm
+		Vector3 reflectedRayDir = new Vector3(normalNorm);
+		reflectedRayDir.scale(2.0);
+		reflectedRayDir.scale(projection);
+		reflectedRayDir.sub(outgoingNorm);
+		reflectedRayDir.normalize();
+		
+		Ray reflectedRay = new Ray();
+		reflectedRay.set(record.location, reflectedRayDir);
+		reflectedRay.makeOffsetRay();
+		Color reflectedColor = new Color();
+		RayTracer.shadeRay(reflectedColor, scene, reflectedRay, depth);
+		reflectedColor.scale(fresnel);
+
+		/* substrate material */
+		substrate.shade(outIntensity, scene, ray, record, depth);
+		
+		outIntensity.scale(1.0-fresnel);
+		outIntensity.add(reflectedColor);
 	}
 }
